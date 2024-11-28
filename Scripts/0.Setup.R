f83aba14d665bd1ead2e8cb7a4f05bcefa1c7a68
Arno Botha <arno.spasie.botha@gmail.com>
2024-11-28 12:18:23 +0200

    Housekeeping of setup-scripts

--- Scripts/0.Setup.R
+++ Scripts/0.Setup.R
@@ -87,71 +87,42 @@ k <- 6 # Probation period
 Username <- Sys.getenv("USERNAME")
 
 # - Setting the paths conditional on the current active user
-if (Username == "WRQ"){ ### - Paths for AB
-  # Custom path where R-scripts are saved
-  path_cust <- "C:/Users/WRQ/OneDrive - FRG/Analytix/Research/Default Survival Modelling/Term-Structure-Modelling-RetailMortgages/Scripts/"
-  
-  # Common path for storing important R-objects as back-up
-  genObjPath <- "C:/Users/WRQ/OneDrive - FRG/Analytix/Research/Default Survival Modelling/Term-Structure-Modelling-RetailMortgages/Objects/"
-  genObjPath_Res <- "C:/Users/WRQ/OneDrive - FRG/Default Survival Modelling/Term-Structure-Modelling-RetailMortgages/Objects/Resampling Schemes/" # Excel sheets for resampling tests
-  
-  # Common path for saving important analytics (e.g., sampling)
-  genFigPath <- "C:/Users/WRQ/OneDrive - FRG/Analytix/Research/Default Survival Modelling/Term-Structure-Modelling-RetailMortgages/Figures/" # General folder path
-  genFigPath_CumHaz <- "C:/Users/WRQ/OneDrive - FRG/Analytix/Research/Default Survival Modelling/Term-Structure-Modelling-RetailMortgages/Figures/Baseline Hazard Comparison/" # Figures of baseline (cumulative) hazard comparison
-  genFigPath_Res <- "C:/Users/WRQ/OneDrive - FRG/Analytix/Research/Default Survival Modelling/Term-Structure-Modelling-RetailMortgages/Figures/Resampling Comparison/Main/" # Figures of resampling tests
-  genFigPath_Res_anc <- "C:/Users/WRQ/OneDrive - FRG/Analytix/Research/Default Survival Modelling/Term-Structure-Modelling-RetailMortgages/Figures/Resampling Comparison/Exploratory/" # Ancillary figures of resampling tests
-  genFigPath_ass <- "C:/Users/WRQ/OneDrive - FRG/Analytix/Research/Default Survival Modelling/Term-Structure-Modelling-RetailMortgages/Figures/Modelling Assumptions/" # Modelling assumptions path
-} else if (Username == "R5532132"){ ### - Paths for MM
-  # Custom path where R-scripts are saved
+if (Username == "Arno Botha"){ # Dr Arno Botha | Kralkatorrik-machine
+  # - Common path for saving large R-objects as back-up and/or as reusable checkpoints
+  genPath <- "E:/DataDump/RetailMortgages-FNB/Term-Structure-Modelling-RetailMortgages/"
+  # - Common path from which raw big datasets are imported
+  genRawPath <- "E:/DataDump/RetailMortgages-FNB/"
+  # - Common path for sourcing R-scripts in main codebase
+  path_cust <- "E:/Backupz/Google Drive/WorkLife/Analytix/R&D Codebases/Default Survival Modelling/Term-Structure-Modelling-RetailMortgages/Scripts/"
+  # - Common path for storing important (but small!) R-objects as back-up
+  genObjPath <- "E:/Backupz/Google Drive/WorkLife/Analytix/R&D Codebases/Default Survival Modelling/Term-Structure-Modelling-RetailMortgages/Objects/"
+  # - Common path for saving important analytics and figures
+  genFigPath <- "E:/Backupz/Google Drive/WorkLife/Analytix/R&D Codebases/Default Survival Modelling/Term-Structure-Modelling-RetailMortgages/Figures/"
+} else if (toupper(Username) == "R5532132"){ # Marcel Muller | Botha-machine
+  # - Common path for saving large R-objects as back-up and/or as reusable checkpoints
+  genPath <- "C:/Data/DefaultSurv_Data/"
+  # - Common path from which raw big datasets are imported
+  genRawPath <- "C:/Data/"
+  # - Common path for sourcing R-scripts in main codebase
   path_cust <- "C:/Users/R5532132/OneDrive - FRG/GCRM/Default Survival Modelling/Term-Structure-Modelling-RetailMortgages/Scripts/"
-  
-  # Common path for storing important R-objects as back-up
-  genObjPath <- "C:/Users/R5532132/OneDrive - FRG/GCRM/Default Survival Modelling/Term-Structure-Modelling-RetailMortgages/Objects/" # General objects"
-  genObjPath_Res <- "C:/Users/R5532132/OneDrive - FRG/GCRM/Default Survival Modelling/Term-Structure-Modelling-RetailMortgages/Objects/Resampling Schemes/" # Excel sheets for resampling tests
-  
-  # Common path for saving important analytics (e.g., sampling)
-  genFigPath <- "C:/Users/R5532132/OneDrive - FRG/GCRM/Default Survival Modelling/Term-Structure-Modelling-RetailMortgages/Figures/" # General folder path
-  genFigPath_CumHaz <- "C:/Users/R5532132/OneDrive - FRG/GCRM/Default Survival Modelling/Term-Structure-Modelling-RetailMortgages/Figures/Baseline Hazard Comparison/" # Figures of baseline (cumulative) hazard comparison
-  genFigPath_Res <- "C:/Users/R5532132/OneDrive - FRG/GCRM/Default Survival Modelling/Term-Structure-Modelling-RetailMortgages/Figures/Resampling Comparison/Main/" # Figures of resampling tests
-  genFigPath_Res_anc <- "C:/Users/R5532132/OneDrive - FRG/GCRM/Default Survival Modelling/Term-Structure-Modelling-RetailMortgages/Figures/Resampling Comparison/Exploratory/" # Ancillary figures of resampling tests
-  genFigPath_ass <- "C:/Users/R5532132/OneDrive - FRG/GCRM/Default Survival Modelling/Term-Structure-Modelling-RetailMortgages/Figures/Modelling Assumptions/" # Modelling assumptions path
-  path_cust <- "C:/Users/R5532132/OneDrive - FRG/Default Survival Modelling/Term-Structure-Modelling-RetailMortgages/Scripts/"
-  
-  # Common path for storing important R-objects as back-up
-  genObjPath <- "C:/Users/R5532132/OneDrive - FRG/Default Survival Modelling/Term-Structure-Modelling-RetailMortgages/Objects/" # General objects"
-  genObjPath_Res <- "C:/Users/R5532132/OneDrive - FRG/Default Survival Modelling/Term-Structure-Modelling-RetailMortgages/Objects/Resampling Schemes/" # Excel sheets for resampling tests
-  
-  # Common path for saving important analytics (e.g., sampling)
-  genFigPath <- "C:/Users/R5532132/OneDrive - FRG/Default Survival Modelling/Term-Structure-Modelling-RetailMortgages/Figures/" # General folder path
-  genFigPath_CumHaz <- "C:/Users/R5532132/OneDrive - FRG/Default Survival Modelling/Term-Structure-Modelling-RetailMortgages/Figures/Baseline Hazard Comparison/" # Figures of baseline (cumulative) hazard comparison
-  genFigPath_Res <- "C:/Users/R5532132/OneDrive - FRG/Default Survival Modelling/Term-Structure-Modelling-RetailMortgages/Figures/Resampling Comparison/Main/" # Figures of resampling tests
-  genFigPath_Res_anc <- "C:/Users/R5532132/OneDrive - FRG/Default Survival Modelling/Term-Structure-Modelling-RetailMortgages/Figures/Resampling Comparison/Exploratory/" # Ancillary figures of resampling tests
-  genFigPath_ass <- "C:/Users/R5532132/OneDrive - FRG/Default Survival Modelling/Term-Structure-Modelling-RetailMortgages/Figures/Modelling Assumptions/" # Modelling assumptions path
-} else if (Username == "R5668395"){ ### - Paths for RB
-  # Custom path where R-scripts are saved
+  # - Common path for storing important (but small!) R-objects as back-up
+  genObjPath <- "C:/Users/R5532132/OneDrive - FRG/GCRM/Default Survival Modelling/Term-Structure-Modelling-RetailMortgages/Objects/"
+  # - Common path for saving important analytics and figures
+  genFigPath <- "C:/Users/R5532132/OneDrive - FRG/GCRM/Default Survival Modelling/Term-Structure-Modelling-RetailMortgages/Figures/"
+} else if (toupper(Username) == "R5668395"){ # Roland Breedt | Botha-machine
+  # - Common path for saving large R-objects as back-up and/or as reusable checkpoints
+  genPath <- "C:/Data/DefaultSurv_Data/"
+  # - Common path from which raw big datasets are imported
+  genRawPath <- "C:/Data/"  
+  # - Common path for storing important (but small!) R-objects as back-up
   path_cust <- "C:/Users/R5668395/Documents/GitHub TermStruc/Term-Structure-Modelling-RetailMortgages/Scripts/"
-  
-  # Common path for storing important R-objects as back-up
-  genObjPath <- "C:/Users/R5668395/Documents/GitHub TermStruc/Term-Structure-Modelling-RetailMortgages/Objects/" # General objects"
-  genObjPath_Res <- "C:/Users/R5668395/Documents/GitHub TermStruc/Term-Structure-Modelling-RetailMortgages/Objects/Resampling Schemes/" # Excel sheets for resampling tests
-  
-  # Common path for saving important analytics (e.g., sampling)
+  # - Common path for storing important (but small!) R-objects as back-up
+  genObjPath <- "C:/Users/R5668395/Documents/GitHub TermStruc/Term-Structure-Modelling-RetailMortgages/Objects/"
+  # - Common path for saving important analytics and figures
   genFigPath <- "C:/Users/R5668395/Documents/GitHub TermStruc/Term-Structure-Modelling-RetailMortgages/Figures/" # General folder path
-  genFigPath_CumHaz <- "C:/Users/R5668395/Documents/GitHub TermStruc/Term-Structure-Modelling-RetailMortgages/Figures/Baseline Hazard Comparison/" # Figures of baseline (cumulative) hazard comparison
-  genFigPath_Res <- "C:/Users/R5668395/Documents/GitHub TermStruc/Term-Structure-Modelling-RetailMortgages/Figures/Resampling Comparison/Main/" # Figures of resampling tests
-  genFigPath_Res_anc <- "C:/Users/R5668395/Documents/GitHub TermStruc/Term-Structure-Modelling-RetailMortgages/Figures/Resampling Comparison/Exploratory/" # Ancillary figures of resampling tests
-  genFigPath_ass <- "C:/Users/R5668395/Documents/GitHub TermStruc/Term-Structure-Modelling-RetailMortgages/Figures/Modelling Assumptions/" # Modelling assumptions path
 } else { ### - If username not found in if statements, execution stops
   stop("User-specific paths not set for current user: ", Username, ". Please fix in setup script (0.Setup.R) before continuing.")
 }
-
-
-# --- General paths
-# - Common path for saving big data objects
-genPath <- "C:/Data/DefaultSurv_Data/"
-
-# - Common path for importing raw data
-genRawPath <- "C:/Data/"
 
 
 
@@ -159,14 +130,14 @@ genRawPath <- "C:/Data/"
 
 # ------ Custom function definitions
 # - Load all custom functions defined in a separate R-script
-source(paste0(path_cust,"0a(i).CustomFunctions.R"))
+source(paste0(path_cust,"0a.CustomFunctions.R"))
 
 # - True End procedure functions defined in a separate R-script
-source(paste0(path_cust,"0a(ii).TruEnd.R"))
+source(paste0(path_cust,"TruEnd.R"))
 
 # - Compile Delinquency Calculation Functions (CD, MD/DoD)
-source(paste0(path_cust,'0a(iii).DelinqM.R'))
+source(paste0(path_cust,'DelinqM.R'))
 
 # - Survival functions
-source(paste0(path_cust,'0a(iv).SurvFunc.R'))
+source(paste0(path_cust,'0b(i).FunkySurv.R'))
 
